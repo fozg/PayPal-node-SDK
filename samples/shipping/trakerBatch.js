daeaa4bd8aa1d58a6c774aa4a7f85e2b6ecc0c9a
fozg <phongdaohong@gmail.com>
2020-04-05 19:18:33 +0700

    update tracker get

--- samples/shipping/trakerBatch.js
+++ samples/shipping/trakerBatch.js
@@ -4,29 +4,38 @@
 var paypal = require("../../");
 require("../configure");
 
-paypal.shipping.trackerBatch.create(
-  {
-    trackers: [
-      {
-        transaction_id: "8MC585209K746392H",
-        tracking_number: "443844607820",
-        status: "SHIPPED",
-        carrier: "FEDEX",
-      },
-      {
-        transaction_id: "53Y56775AE587553X",
-        tracking_number: "443844607821",
-        status: "SHIPPED",
-        carrier: "FEDEX",
-      },
-    ],
-  },
-  function (error, sale) {
-    if (error) {
-      throw error;
-    } else {
-      console.log("Add traker Response");
-      console.log(JSON.stringify(sale));
-    }
+// paypal.shipping.trackerBatch.create(
+//   {
+//     trackers: [
+//       {
+//         transaction_id: "8MC585209K746392H",
+//         tracking_number: "443844607820",
+//         status: "SHIPPED",
+//         carrier: "FEDEX",
+//       },
+//       {
+//         transaction_id: "53Y56775AE587553X",
+//         tracking_number: "443844607821",
+//         status: "SHIPPED",
+//         carrier: "FEDEX",
+//       },
+//     ],
+//   },
+//   function (error, sale) {
+//     if (error) {
+//       throw error;
+//     } else {
+//       console.log("Add traker Response");
+//       console.log(JSON.stringify(sale));
+//     }
+//   }
+// );
+
+paypal.shipping.tracker.get("53Y56775AE587553X-443844607821", function (error, sale) {
+  if (error) {
+    throw error;
+  } else {
+    console.log("Add traker Response");
+    console.log(JSON.stringify(sale));
   }
-);
+});
